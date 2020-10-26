0a496ea4e2c1d6d2960857be495366ddcfe364e9
Mike Chabot <mike.chabot@live.com>
2020-10-26 01:56:46 -0400

    Add mounting/unmounting

--- src/mikechabot-root-config.ts
+++ src/mikechabot-root-config.ts
@@ -3,7 +3,12 @@ import { registerApplication, start } from "single-spa";
 registerApplication({
   name: "@mikechabot/player-card",
   app: () => System.import("@mikechabot/player-card"),
-  activeWhen: ["/"],
+  activeWhen: (location) => {
+    // eslint-disable-next-line no-console
+    console.log(location);
+    return true;
+  },
+  customProps: { customProp1: "foobar!" },
 });
 
 start({
