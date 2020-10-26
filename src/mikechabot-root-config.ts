bba2d9b4a44fab95b9279c0ee2e7b377c79f368a
Mike Chabot <mike.chabot@live.com>
2020-10-25 23:04:14 -0400

    Update root config

--- src/mikechabot-root-config.ts
+++ src/mikechabot-root-config.ts
@@ -2,7 +2,7 @@ import { registerApplication, start } from "single-spa";
 
 registerApplication({
   name: "@mikechabot/player-card",
-  app: () => System.import("//localhost:8080/mikechabot-player-card.js"),
+  app: () => System.import("@mikechabot/player-card"),
   activeWhen: ["/"],
 });
 
